29fd3cba67e6e4da001e50b04a27618f643a5549
吉田遥音 <haruto@MBP-14.local>
2024-04-29 14:20:22 +0900

    update __init__.py

---
+++ src/rye_sample/__init__.py
@@ -0,0 +1,14 @@
+"""A sample module for the rye package.
+
+This module contains a single function that returns a greeting message.
+"""
+
+
+def hello() -> str:
+    """Return a greeting message.
+
+    Returns:
+        str: A greeting message.
+
+    """
+    return "Hello from rye-sample!"
